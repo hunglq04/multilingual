2bcb33108575cd1a976f848006fb284e792214b2
hlq <hlq@elca.vn>
2020-08-11 11:20:09 +0700

    Add router view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/projects', pathMatch: 'full'},
+  {path: 'projects', component: ProjectListComponent},
+  {path: 'project/:id', component: ProjectDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
